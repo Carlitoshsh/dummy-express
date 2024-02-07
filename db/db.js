c4609affd6ea4446deecfd3f6246412e5f02a1dc
Carlos Gómez <caragomezher@unal.edu.co>
2024-02-06 22:34:37 -0500

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,12 +1,7 @@
 const fs = require('fs');
 const { Pool } = require('pg');
-const path = './db_settings.json';
 
-// Check if the file exists
-fs.access(path, fs.constants.F_OK, (err) => {
-  if (err) {
-    console.error('The db_settings.json file does not exist. Using process.env for database settings.');
-    // Create a pool of connections using process.env
+    // Create a pool of connections using dbSettings
     const pool = new Pool({
       user: process.env.DB_USER,
       host: process.env.DB_HOST,
@@ -18,22 +13,3 @@ fs.access(path, fs.constants.F_OK, (err) => {
       },
     });
     module.exports = pool;
-  } else {
-    // The file exists, proceed with loading its content
-    const dbSettings = require(path);
-    console.log('Database settings loaded successfully:', dbSettings);
-
-    // Create a pool of connections using dbSettings
-    const pool = new Pool({
-      user: process.env.DB_USER || dbSettings.user,
-      host: process.env.DB_HOST || dbSettings.host,
-      database: process.env.DB_NAME || dbSettings.database,
-      password: process.env.DB_PWD || dbSettings.password,
-      port: process.env.DB_PORT || dbSettings.port,
-      ssl: {
-        rejectUnauthorized: false, // Set to true if you have a valid CA certificate
-      },
-    });
-    module.exports = pool;
-  }
-});
