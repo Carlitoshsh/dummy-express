98e85715e0070c58bb96ef370ec4c29aef843614
Carlos Gómez <caragomezher@unal.edu.co>
2024-09-03 20:38:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ def get_students():
 
     # Fetch all the rows returned by the query
     rows = cur.fetchall()
+    cur.close()
 
     return rows
 
 # Close the cursor and the connection
-cur.close()
 conn.close()
